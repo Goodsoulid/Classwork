c75b2bea83981440e2f59b13b0727d11018cb10b
Goodsoulid <Frozon1994@gmail.com>
2022-10-10 22:00:02 +0300

    Lesson8 update new

--- app/src/main/java/com/example/classwork/Lesson8.java
+++ app/src/main/java/com/example/classwork/Lesson8.java
@@ -3,6 +3,23 @@ package com.example.classwork;
 public class Lesson8 {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        try {
+            getWrongValue(null);
+        } catch (ArithmeticException e) {
+            System.out.println("caught arithmetic");
+        } catch (NullPointerException e) {
+            System.out.println("caught nullPointException");
+
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(" caught indexOutOfBound");
+
+        } finally {
+            System.out.println("do if success or exception thrown");
+        }
+
+    }
+
+    static void getWrongValue(Integer value) {
+        System.out.println(value.toString());
     }
 }
--- app/src/main/java/com/example/classwork/Lesson8_2.java
+++ app/src/main/java/com/example/classwork/Lesson8_2.java
@@ -1,5 +1,27 @@
 package com.example.classwork;
 
-public class Lesson8_2 {
+import java.io.FileNotFoundException;
 
+public class Lesson8_2 {
+    public static void main(String[] args) {
+        try {
+            throwUnCheckedException();
+        }catch (NullPointerException e){
+            System.out.println(e.getMessage());
+        }
+//        try {
+//            throwCheckedException();
+//        }catch (Exception e){
+//            System.out.println(e.getMessage());
+//        }
+
+    }
+
+    static void throwUnCheckedException() throws NullPointerException {
+        throw new NullPointerException("runTime");
+    }
+
+    static void throwCheckedException() throws FileNotFoundException {
+        throw new FileNotFoundException("compileTime");
+    }
 }
