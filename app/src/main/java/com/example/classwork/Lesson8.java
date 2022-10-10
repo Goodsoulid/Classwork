d9fe694b204661aa7cb9d1cb347ff31aab4c9acc
Goodsoulid <Frozon1994@gmail.com>
2022-10-10 19:51:47 +0300

    Lesson8 update

--- app/src/main/java/com/example/classwork/Lesson8.java
+++ app/src/main/java/com/example/classwork/Lesson8.java
@@ -3,6 +3,6 @@ package com.example.classwork;
 public class Lesson8 {
 
     public static void main(String[] args) {
-
+        System.out.println("Hello world");
     }
 }
--- app/src/main/java/com/example/classwork/Lesson8_2.java
+++ app/src/main/java/com/example/classwork/Lesson8_2.java
@@ -1,4 +1,5 @@
 package com.example.classwork;
 
 public class Lesson8_2 {
+
 }
